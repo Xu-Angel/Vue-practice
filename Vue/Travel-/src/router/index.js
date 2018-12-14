6a09772f277d025c0b4c32ae06862418f51c6adf
Angel <1049547382@qq.com>
2018-12-14 11:36:42 +0800

    add pages && change router

--- Vue/Travel-/src/router/index.js
+++ Vue/Travel-/src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Home from '@/pages/home/home'
+import List from '@/pages/list/list'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+   /*  {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    } */
+    {
+      path: '/',
+      name: 'Lome',
+      component: Home
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
