63a49d7ee2cdafd28210b3f3d3b9c117cc7bb73a
Angel <1049547382@qq.com>
2018-12-14 11:50:29 +0800

    router-lint && 根容器

--- Vue/Travel-/src/pages/home/home.vue
+++ Vue/Travel-/src/pages/home/home.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>home</div>
+  <div>
+    <div class="home">home</div>
+    <router-link to="/list">列表页</router-link>
+  </div>
+  <!--
+    <div class="home">home</div>
+    <router-link to="/list">列表页</router-link>
+   -->
+   <!-- 被注释的写法 会报错  要有个根容器包裹 -->
 </template>
 <script>
 export default {
@@ -7,7 +15,9 @@ export default {
 }
 </script>
 <style>
-
+  .home {
+    font-size: 50px;
+  }
 </style>
 
 
