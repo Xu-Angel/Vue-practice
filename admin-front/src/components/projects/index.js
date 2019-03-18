95f3c0833e2a9220c16dde1b5871b23fb8a2e9cb
Angel <1049547382@qq.com>
2019-03-18 15:46:54 +0800

    neaten

--- admin-front/src/components/projects/index.js
+++
@@ -1,8 +0,0 @@
-const config = {
-	'currentProject': 'template',
-	//'page': require('./pro_page/.config.js'),
-	'template': require('./template/.config.js')
-}
-//console.log(config[config.currentProject])
-
-module.exports = config[config.currentProject];
