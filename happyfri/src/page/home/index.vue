39c201832011cd2127d43d8b2db7f208e149472b
Angel <1049547382@qq.com>
2019-09-09 15:50:14 +0800

    review Vue

--- happyfri/src/page/home/index.vue
+++ happyfri/src/page/home/index.vue
@@ -1,16 +1,21 @@
 <template>
+<div>
+      <v-home></v-home>
   	<div class="home_container">
         <itemcontainer father-component="home"></itemcontainer>
+    </div>
     </div>
 </template>
 
 <script>
 import itemcontainer from '../../components/itemcontainer'
+import home from '../../components/Home'
 
 export default {
 	name: 'home', // 页面路由的名字
   	components: {
-  		itemcontainer
+      itemcontainer,
+      'v-home': home
   	},
     created(){
 
