70df57c465dac96a80274e5692ba2f5cf818b2b5
Xu-Angel <1049547382@qq.com>
2020-04-24 17:12:50 +0800

    开启namespaced

--- vuexlearn/src/page/counter/counterStore.js
+++ vuexlearn/src/page/counter/counterStore.js
@@ -8,10 +8,14 @@ Vue.use(Vuex)
 // A Vuex instance is created by combining the state, mutations, actions,
 // and getters.
 export default {
+  // https://vuex.vuejs.org/zh/guide/modules.html
+  namespaced: true, //!! 不用使用true 使得，模块内部的 action、mutation 和 getter 是注册在全局命名空间的——这样使得多个模块能够对同一 mutation 或 action 作出响应。
   // root state object.
 // each Vuex instance is just a single state tree.
+// 在module情況下 自动被分配 需要加上module名才可访问，但是在本内部内则不用
   state: {
-    counts: 0
+    counts: 0,
+    hi: 'ddd'
   },
   // mutations are operations that actually mutate the state.
 // each mutation handler gets the entire state tree as the
@@ -31,7 +35,8 @@ export default {
   actions: {
     increment: ({commit}) => commit('increment'),
     decrement: ({commit}) => commit('decrement'),
-    incrementIfOdd: ({ commit, state }) => {
+    incrementIfOdd: ({ commit, state, rootState }) => {
+      console.log(';hi  i root ', rootState);
       if ((state.counts + 1) % 2 === 0) {
         commit('increment')
       }
@@ -43,7 +48,7 @@ export default {
   },
   // getters are functions.
   getters: {
-    evenOrOdd: state => state.count % 2 === 0 ? 'even' : 'odd'
+    evenOrOdd: state => state.counts % 2 === 0 ? 'even' : 'odd'
   }
 }
 
--- vuexlearn/src/page/counter/index.vue
+++ vuexlearn/src/page/counter/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <p>current score {{$store.state.moduleCounter.counts }}, count is {{evenOrOdd}}</p>
+    <p>current score {{counts }}, count is {{evenOrOdd}}</p>
+    <p>{{hi}}</p>
     <button @click="increment">+</button>
     <button @click="decrement">-</button>
     <button @click="incrementIfOdd">Increment if odd</button>
@@ -9,13 +10,65 @@
 </template>
 
 <script>
-import {mapGetters, mapActions} from 'vuex'
+// import {mapGetters, mapState, mapActions} from 'vuex'
+
+// 方式三
+import { createNamespacedHelpers } from 'vuex'
+const { mapState, mapActions, mapGetters } = createNamespacedHelpers('moduleCounter')
+//
 export default {
-  computed: mapGetters(['evenOrOdd']),
-  methods: mapActions(['increment',
-    'decrement',
-    'incrementIfOdd',
-    'incrementAsync'])
+  /* 方式一 */
+//   computed: {
+//     ...mapState({
+//       hi: state => state.moduleCounter.hi,
+//       counts: state => state.moduleCounter.counts
+//     }),
+//     ...mapGetters({
+//       'evenOrOdd': 'moduleCounter/evenOrOdd'
+//     })
+//     },
+//   methods: {
+//     ...mapActions({
+//       'increment': 'moduleCounter/increment',
+//     'decrement': 'moduleCounter/decrement',
+//     'incrementIfOdd': 'moduleCounter/incrementIfOdd',
+//     'incrementAsync': 'moduleCounter/incrementAsync'})
+//     }
+// }
+/* 方式二 */
+  // computed: {
+  //   ...mapState('moduleCounter',{
+  //     hi: state => state.hi,
+  //     counts: state => state.counts
+  //   }),
+  //   ...mapGetters('moduleCounter',{
+  //     'evenOrOdd': 'evenOrOdd'
+  //   })
+  //   },
+  // methods: {
+  //   ...mapActions('moduleCounter', {
+  //     'increment':  'increment',
+  //   'decrement': 'decrement',
+  //   'incrementIfOdd': 'incrementIfOdd',
+  //   'incrementAsync': 'incrementAsync'})
+  //   }
+  /* 方式三 */
+    computed: {
+    ...mapState({
+      hi: state => state.hi,
+      counts: state => state.counts
+    }),
+    ...mapGetters({
+      'evenOrOdd': 'evenOrOdd'
+    })
+    },
+  methods: {
+    ...mapActions({
+      'increment':  'increment',
+    'decrement': 'decrement',
+    'incrementIfOdd': 'incrementIfOdd',
+    'incrementAsync': 'incrementAsync'})
+    }
 }
 </script>
 
