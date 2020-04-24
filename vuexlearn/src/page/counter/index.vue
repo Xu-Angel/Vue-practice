cb501ccf40f904522e99d93f82b2b57e35ee2804
Xu-Angel <1049547382@qq.com>
2020-04-24 16:12:15 +0800

    neaten floder;init vuex

---
+++ vuexlearn/src/page/counter/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>current score {{$store.state.moduleCounter.counts }}, count is {{evenOrOdd}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">Increment if odd</button>
+    <button @click="incrementAsync">Increment async</button>
+  </div>
+</template>
+
+<script>
+import {mapGetters, mapActions} from 'vuex'
+export default {
+  computed: mapGetters(['evenOrOdd']),
+  methods: mapActions(['increment',
+    'decrement',
+    'incrementIfOdd',
+    'incrementAsync'])
+}
+</script>
+
+<style>
+
+</style>
